77e933ea56759b50c29d0c57f2a12047a7f46d32
nasodev <creationand2017@gmail.com>
2025-02-23 16:22:10 +0900

    이미지 최적화

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -7,7 +7,13 @@ const Logo = () => {
     return (
         <Link href="/" className="flex items-center text-dark dark:text-light">
             <div className="w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-light mr-2 md:mr-4">
-                <Image src={profileImg} alt="FunDev" className="w-full h-auto rounded-full"></Image>
+                <Image
+                    src={profileImg}
+                    alt="FunDev"
+                    className="w-full h-auto rounded-full"
+                    sizes="33vw"
+                    priority
+                ></Image>
             </div>
             <span className="font-bold text-lg md:text-xl dark:font-semibold">FunDev</span>
         </Link>
