f70d354afe9bcf30d6b57d40b86ebdbc8a600ec9
nasodev <creationand2017@gmail.com>
2026-09-07 18:57:25 +0900

    fix: put the contact form first on mobile

--- app/(ko)/(about)/contact/page.tsx
+++ app/(ko)/(about)/contact/page.tsx
@@ -31,10 +31,10 @@ export const metadata = {
 export default function Contact() {
     return (
         <section className="w-full h-auto md:min-h-[75vh] border-b-2 border-solid border-dark dark:border-light flex flex-col md:flex-row items-center justify-center text-dark dark:text-light">
-            <div className="inline-block w-full sm:w-4/5 md:w-2/5 h-full md:border-r-2 border-solid border-dark dark:border-light">
+            <div className="hidden md:block md:w-2/5 h-full md:border-r-2 border-solid border-dark dark:border-light">
                 <LottieAnimation />
             </div>
-            <div className="w-full sm:w-3/5 flex flex-col items-start justify-center px-5 xs:px-10 md:px-16 pb-8">
+            <div className="w-full sm:w-3/5 flex flex-col items-start justify-center px-5 xs:px-10 md:px-16 py-8">
                 <h1 className="font-bold text-2xl xs:text-3xl sm:text-4xl">문의하기</h1>
                 <ContactForm />
             </div>
